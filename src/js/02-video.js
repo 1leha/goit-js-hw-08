2bef86b8f03f2cec452e2c120408a01c1a27067a
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-06-22 16:13:24 +0400

    adding second task wo localeStorage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
 
+const iframe = document.getElementById('vimeo-player');
+
+// console.log(iframe);
+// console.dir(iframe);
+const options = {
+  autoplay: true,
+  color: 'ff0000',
+  muted: true,
+  //   controls: false,
+};
+// console.log(options);
+
+const player = new Player(iframe, options);
+console.log(player);
+player.seconds = 50;
+
+player.on('timeupdate', ({ seconds }) => {
+  console.log(seconds);
+});
+// player.play();
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
