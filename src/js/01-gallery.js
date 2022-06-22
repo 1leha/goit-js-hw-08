468deabbb98dbd846af02c9f7f6ac6c783f11c54
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-06-22 14:37:02 +0400

    adding First task - gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+renderGallery(galleryItems);
+
+const imageLightBox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlay: true,
+});
+
+function renderGallery(gallery) {
+  refs.gallery.innerHTML = gallery.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`,
+    ''
+  );
+}
