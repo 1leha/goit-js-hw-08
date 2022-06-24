165cf0e7e1673b6542e2664e06f7d802c63898d2
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-06-24 18:44:28 +0400

    adding 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('[name="email"]');
+const message = form.querySelector('[name="message"]');
+
+const FORM_STATE = 'feedback-form-state';
+const savedOptions = {};
+
+document.addEventListener('load', onReload());
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onSubmit);
+// console.dir(form);
+
+function onFormInput(e) {
+  savedOptions[e.target.name] = e.target.value;
+  localStorage.setItem(FORM_STATE, JSON.stringify(savedOptions));
+}
+
+function onReload() {
+  if (localStorage.getItem(FORM_STATE)) {
+    const loadedOptions = JSON.parse(localStorage.getItem(FORM_STATE));
+
+    savedOptions.email = email.value = loadedOptions.email;
+    savedOptions.message = message.value = loadedOptions.message;
+  }
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  form.reset();
+  localStorage.removeItem(FORM_STATE);
+}
